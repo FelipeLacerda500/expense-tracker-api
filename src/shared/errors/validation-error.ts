d4beec2a0dc48317d267f8a60885471c31969ad7
Felipe Lacerda <felipelacerda500@gmail.com>
2024-08-07 20:21:47 -0300

    feat: add ValidationError

---
+++ src/shared/errors/validation-error.ts
@@ -0,0 +1,10 @@
+import { IParamErrors } from '../protocols';
+
+export class ValidationError extends Error {
+  constructor(
+    message?: string,
+    private readonly params?: IParamErrors,
+  ) {
+    super(message || 'Os dados fornecidos são inválidos.');
+  }
+}
